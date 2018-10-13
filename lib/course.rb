f6ba663541e3fa49f49e295c3b1211bcbc4a2aa0
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-13 17:32:39 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,10 +1,13 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
 
   def initialize
     @title = title
     @schedule = schedule
     @description = description
   end
+
+  
 
 end
